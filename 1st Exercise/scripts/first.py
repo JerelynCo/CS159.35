fddf02cd49b76ce304c6b5723b2ec20fdb19bbec
JerelynCo <jerelynco@gmail.com>
2014-11-20 16:46:39 +0800

    First two lessons

---
+++ 1st Exercise/scripts/first.py
@@ -0,0 +1,19 @@
+from numpy import *
+from matplotlib import pylab as plt
+
+xvar = array([1,2,3,4,5])
+yvar = xvar*2
+yvar2 = xvar*4
+
+plt.clf()
+plt.plot(xvar,yvar,'rx')
+plt.plot(xvar,yvar2,'rx')
+
+plt.xlim((0,6))
+plt.ylim((0,12))
+
+plt.xlabel("variable X")
+plt.ylabel("variable Y")
+plt.show()
+plt.title("My First Python Plot")
+#plt.savefig("../figures/fig_first_plot2.png")
